a65271152307e804f634712287bb2dfb03bc5e79
ant-coh <antoine.cohic@gmail.com>
2025-11-10 18:29:44 -0500

    Removed unrelevant markers

--- kinectome.m
+++ kinectome.m
@@ -12,6 +12,11 @@ load Acc.mat
 nbp=size(Acc,2);
 Kinect=cell(3,nbp);
 MAC=cell(3,nbp);
+sil=cell(3,nbp);
+
+S=[1 1 1 1 2 2 1 3 3 1 1 3 3 3 3 3 2 2 2 2 2];
+[S_sort,I]=sort(S);
+nbclust=max(S_sort);
 
 for p=1:nbp
     if isempty(Acc{1,p})
@@ -27,11 +32,15 @@ for p=1:nbp
             ahap=mean(ahead(2:2:end,:),1);                                  % Acc. medlat et antpos moyennes de la tête
             atemp(1:8,:)=[];
             a_glob=[ahml;ahap;atemp];
+
+            a_glob(53:54,:)=[];                                             % Suppression LHEE et RHEE
+            a_glob(41:42,:)=[];
+            a_glob(29:32,:)=[];                                             % Suppression LPSI RPSI
+            a_glob(7:12,:)=[];                                              % Suppression CLAV, STRN, RBAK
+
             a_medlat=a_glob(1:2:end,:);
             a_antpos=a_glob(2:2:end,:);
-
             
-
             k_medlat=corrcoef(a_medlat');                                   % Kinectome par corrélation de Pearson
             k_antpos=corrcoef(a_antpos');
             Kinect{c,p}{cy,2}=k_medlat;
@@ -52,13 +61,37 @@ for p=1:nbp
         clear Kap Kml
         nbm=size(k_antpos,1);
         mac=[nbm,2];                                                        % Mean absolute corrrelation
-        for node=1:nbm
-            mac(node,1)=(sum(abs(Kinect{c,p}{end-1,1}(node,:)))-1)/nbm;
-            mac(node,2)=(sum(abs(Kinect{c,p}{end-1,2}(node,:)))-1)/nbm;
+        silhou=zeros(1,nbm);
+
+        for i=1:nbm
+            mac(i,1)=(sum(abs(Kinect{c,p}{end-1,1}(i,:)))-1)/nbm;
+            mac(i,2)=(sum(abs(Kinect{c,p}{end-1,2}(i,:)))-1)/nbm;
+            
+            Kin=Kinect{c,p}{end-1,1}(I,I);
+            clust_i=S_sort(i);
+            ind=1;
+            temp=S_sort;
+            dist=Kin(i,:);
+            dist(i)=[];
+            temp(i)=[];
+            b=zeros(1,2);
+            for cl=1:nbclust
+                if cl==clust_i
+                    v=dist(temp==clust_i);
+                    a=1-mean(v);
+                else
+                    v=dist(temp==cl);
+                    b(ind)=1-mean(v);
+                    ind=ind+1;
+                end
+            end
+            silhou(i)=(min(b)-a)/max(a,min(b));
         end
         MAC{c,p}=mac;
+        sil{c,p}=mean(silhou,2);
     end
 end
 
 save Kinect.mat Kinect
 save MAC.mat MAC
+save sil.mat sil
