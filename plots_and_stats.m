803ab11ca294d3afc5a7d4de86dadf6bf70d4852
ant-coh <antoine.cohic@gmail.com>
2026-01-20 12:11:48 -0500

    Added the Posthoc structure to store results

--- plots_and_stats.m
+++ plots_and_stats.m
@@ -171,16 +171,22 @@ spmi.plot('plot_threshold_label',false,'plot_p_values',true,'autoset_ylim',true)
 %% Post Hoc
 
 clear
-paire=2; % 1 : Knee/Hip, 2 : Ankle/Knee
+clc
+
+paire=1; % 1 : Knee/Hip, 2 : Ankle/Knee
 idms=0;  % 1 : MARP, 0 : DP
 maxd=4;  % Max effect size displayed on colorbar
+thres=0; % Min size of displayed clusters
 
 load PA_CRP.mat
 load participants.mat
+if isfile('Posthoc.mat')
+    load('Posthoc.mat');
+end
 nbp=size(PA_CRP,2);
 
 %--------------------------------------------------------------------------
- 
+
 ind=1;
 idp=0;
 for p=1:nbp
@@ -213,6 +219,8 @@ subplot(133);  spmi.plot();  title('Normality test')
 
 cond=["Even" "Medium" "High"];
 group=["Adultes" "Adolescents" "Enfants" "Jeunes Enfants"];
+fields=["KH_DP" "KH_MARP" ; "AK_DP" "AK_MARP"];
+
 ind=ones(3,4);
 CRPmp=cell(3,4);
 for p=1:nbp
@@ -220,17 +228,17 @@ for p=1:nbp
         continue
     end
     idg=participants{p,3};
-    for c=1:3
+    for c=1:3                                                               % Tri par participant et condition
         CRPtemp(1,:)=PA_CRP{c,p}{end-idms,4}(paire,:);                      % jambe gauche
         CRPtemp(2,:)=PA_CRP{c+3,p}{end-idms,4}(paire,:);                    % jambe droite
-        CRPmp{c,idg}(ind(c,idg),:)=mean(CRPtemp,1);
+        CRPmp{c,idg}(ind(c,idg),:)=mean(CRPtemp,1);                         % Moyenne
         ind(c,idg)=ind(c,idg)+1;
     end
 end
 CRPm=cell(3,4);
 for i=1:3
     for j=1:4
-        CRPm{i,j}=mean(CRPmp{i,j},1);
+        CRPm{i,j}=mean(CRPmp{i,j},1);                                       % Moyenne et std par groupe et condition
         CRPm{i,j}(2,:)=std(CRPmp{i,j},0,1);
     end
 end
@@ -238,7 +246,7 @@ end
 figure
 tl=tiledlayout(3,4);
 c=[1 2 3];
-cpaire=nchoosek(c,2);
+cpaire=nchoosek(c,2);                                                       % Paires de conditions
 cms=colormap(nebula(3));
 cms=brighten(cms,-.2);
 for i=1:3
@@ -258,11 +266,22 @@ rng("shuffle")
         % max may yield qualitatively different results, especially if the
         % effect size is small. Here the max number of iterations is around
         % 30 000, which is fine to run, but if this number was greater, we
-        % could fix iterations at 10 000
+        % could fix iterations at a lower number
+        
+        subfield=group(1,g)+"_"+cond(1,cpaire(i,1))+"_"+cond(1,cpaire(i,2));
+        subfield=matlab.lang.makeValidName(subfield);
+        Posthoc.(fields(paire,idms+1)).Condition.(subfield).NbClusters = ti.nClusters;
+        if ti.nClusters ~=0
+             Posthoc.(fields(paire,idms+1)).Condition.(subfield).p = ti.p;
+             for cl=1:ti.nClusters
+                 Posthoc.(fields(paire,idms+1)).Condition.(subfield).Clusters{cl} = ti.clusters{1,cl}.endpoints;
+             end
+        end
+        Posthoc.(fields(paire,idms+1)).Condition.(subfield).dCohen=cohensd(ti,Y0,Y1,thres);
 
         nexttile
         hold on
-        rect_color(ti,Y0,Y1,0,maxd)
+        rect_color(ti,Y0,Y1,thres,maxd)                                     % Clusters et Cohen's d
         for k=1:2
             plot(0:1:100,CRPm{cpaire(i,k),g}(1,:),'Color',cms(cpaire(i,k),:),'LineWidth',2.5)
             f=fill([0:1:100 100:-1:0],[(CRPm{cpaire(i,k),g}(1,:)+CRPm{cpaire(i,k),g}(2,:)) fliplr((CRPm{cpaire(i,k),g}(1,:)-CRPm{cpaire(i,k),g}(2,:)))],'c');
@@ -338,9 +357,20 @@ for c=1:3
             ti=t.inference(0.05/bc,'two_tailed',true,'iterations',60000,'force_iterations',true);
         end
 
+        subfield=cond(1,c)+"_"+group(1,gpaire(i,1))+"_"+group(1,gpaire(i,2));
+        subfield=matlab.lang.makeValidName(subfield);
+        Posthoc.(fields(paire,idms+1)).Group.(subfield).NbClusters = ti.nClusters;
+        if ti.nClusters ~=0
+             Posthoc.(fields(paire,idms+1)).Group.(subfield).p = ti.p;
+             for cl=1:ti.nClusters
+                 Posthoc.(fields(paire,idms+1)).Group.(subfield).Clusters{cl} = ti.clusters{1,cl}.endpoints;
+             end
+        end
+        Posthoc.(fields(paire,idms+1)).Group.(subfield).dCohen=cohensd(ti,Y0,Y1,thres);
+
         nexttile
         hold on
-        rect_color(ti,Y0,Y1,0,maxd)
+        rect_color(ti,Y0,Y1,thres,maxd)
         for k=1:2
             plot(0:1:100,CRPm{c,gpaire(i,k)}(1,:),'Color',cms(gpaire(i,k),:),'LineWidth',2.5)
             f=fill([0:1:100 100:-1:0],[(CRPm{c,gpaire(i,k)}(1,:)+CRPm{c,gpaire(i,k)}(2,:)) fliplr((CRPm{c,gpaire(i,k)}(1,:)-CRPm{c,gpaire(i,k)}(2,:)))],'c');
@@ -391,6 +421,37 @@ elseif paire==2 && idms==0
     title(tl,"Post-Hoc tests - Ankle/Knee DP",'FontWeight','bold')
 end
 tl.Padding = 'compact'; tl.TileSpacing = 'compact';
+
+function d=cohensd(ti,Y0,Y1,thres)
+    d=zeros(1,ti.nClusters);
+    for c=1:ti.nClusters
+        b_inf=round(ti.clusters{1,c}.endpoints(1,1));
+        b_sup=round(ti.clusters{1,c}.endpoints(1,2));
+        if b_sup-b_inf < thres
+            continue
+        end
+        dtemp=zeros(1,b_sup-b_inf+1);
+        ind=1;
+
+        for b=b_inf+1:b_sup+1
+    
+            mY0=mean(Y0(:,b));                                                  % Mean of the distribution
+            sY0=std(Y0(:,b));                                                   % Standard deviation
+            nY0=size(Y0(:,b),1);                                                % Sample size
+    
+            mY1=mean(Y1(:,b));
+            sY1=std(Y1(:,b));
+            nY1=size(Y1(:,b),1);
+    
+            s_pooled=sqrt(((sY0^2*(nY0-1))+(sY1^2*(nY1- 1)))/(nY0+nY1-2));
+            dtemp(ind)=abs(mY1-mY0)/s_pooled;                                       % Effect size (Cohen's d)
+            ind=ind+1;
+        end
+        d(1,c)=mean(dtemp);
+    end
+end
+
+save Posthoc.mat Posthoc
 
 %% Plan de covariation
 
